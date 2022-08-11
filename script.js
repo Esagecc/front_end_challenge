99966f6d6906ca5089ef905ac2f921392f82c1c4
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-08-11 15:38:40 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let inputName = document.querySelector('#input-name')
+let inputNums = document.querySelector('#input-nums')
+let inputMm = document.querySelector('#input-mm')
+let inputYy = document.querySelector('#input-yy')
+let inputCvc = document.querySelector('#input-cvc')
+let confirmButton = document.querySelector('button')
+
